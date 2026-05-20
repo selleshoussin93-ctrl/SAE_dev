facccd80ba587cfc1e88eebb0eb398e161d9fd0f
Houssin Selles <hselles@iut.univ-paris8.fr>
2026-05-20 15:17:21 +0200

    changement de nom des attributs listeEnnemi et listePoisson

--- src/main/java/universite_paris8/iut/jbouguerba/sae_jeux/modele/Environnement.java
+++ src/main/java/universite_paris8/iut/jbouguerba/sae_jeux/modele/Environnement.java
@@ -15,20 +15,20 @@ public class Environnement {
 
 
     private ArrayList<PoissonDeffense> PoissonsDe;
-    private ArrayList<PoissonAttaque> PoissonsAttaques;
+    private ArrayList<PoissonAttaque> PoissonsAtt;
     private int ressources;
 
     public Environnement(int largeur, int hauteur) {
         this.hauteur = map.length;
         this.largeur = map[0].length;
 
-        this.listeTour = new ArrayList<>();
-        this.listeEnnemi = new ArrayList<>();
+        this.PoissonsDe = new ArrayList<>();
+        this.PoissonsAtt = new ArrayList<>();
         this.ressources = 40;
 
-        listeEnnemi.add(new PoissonAttaque("Requin Basic", 50, 50, "aucun", 6, 0, 10, 1.0));
-        listeEnnemi.add(new PoissonAttaque("Requin Marteau", 30, 30, "rapide", 6, 1, 15, 2.0));
-        listeEnnemi.add(new PoissonAttaque("Requin Baleine", 100, 100, "resistant", 6, 2, 30, 0.5));
+        PoissonsAtt.add(new PoissonAttaque("Requin Basic", 50, 50, "aucun", 6, 0, 10, 1.0));
+        PoissonsAtt.add(new PoissonAttaque("Requin Marteau", 30, 30, "rapide", 6, 1, 15, 2.0));
+        PoissonsAtt.add(new PoissonAttaque("Requin Baleine", 100, 100, "resistant", 6, 2, 30, 0.5));
 
 
     }
@@ -43,8 +43,8 @@ public class Environnement {
     public int getHauteur() {
         return map.length;
     }
-    public ArrayList<PoissonAttaque> getListeEnnemi() {
-        return listeEnnemi;
+    public ArrayList<PoissonAttaque> getListePossonsAttaque() {
+        return PoissonsAtt;
     }
 
 
