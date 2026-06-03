1e82013934736ecaa6c000d379a2c4d4194edaa0
Houssin Selles <hselles@iut.univ-paris8.fr>
2026-06-03 12:09:26 +0200

    correction de la methode toucher de la class poisson attaque

--- src/main/java/universite_paris8/iut/jbouguerba/sae_jeux/modele/PoissonAttaque.java
+++ src/main/java/universite_paris8/iut/jbouguerba/sae_jeux/modele/PoissonAttaque.java
@@ -88,15 +88,16 @@ public class PoissonAttaque extends Poisson {
 
     public void toucher(Bulle projectile, PoissonAttaque requin) {
 
-        double distance = Math.abs(projectile.getX() - requin.getX());
-        System.out.println("Distance bulle/requin : " + distance);
+        double distanceX = Math.abs(projectile.getX() - requin.getX());
 
-        if (distance < 57) {
-            System.out.println("Collision détectée ! Pouvoir : " + projectile.getPouvoir());
+        // Comparer les lignes plutôt que les pixels Y
+        int ligneBulle = (int)(projectile.getY() / 114);
+        int ligneRequin = (int)(requin.getY() / 114);
+
+        if (distanceX < 57 && ligneBulle == ligneRequin) {
             enleveVie(projectile);
 
             if (projectile.getPouvoir().equals("gele")) {
-                System.out.println("Ralentissement !");
                 this.ralentir();
             }
         }
